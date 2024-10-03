6a4034a540d755601d28cdfa905359feee10ac16
samuelcoder1 <echeverrysamuel74@gmail.com>
2024-10-03 16:08:12 -0500

    Create application properties and branch develop

---
+++ src/test/java/com/riwi/RiwiTech/RiwiTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riwi.RiwiTech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiwiTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
